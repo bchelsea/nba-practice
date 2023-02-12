92fad0fc6e1fad039f4ee9afe0822c2aa1ae01fe
Chelsea Barocio <cbarocio@C02C90M5MD6V.local>
2023-02-11 22:54:01 -0800

    adding container to banner

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,9 @@ interface AppProps {
 export const App: React.FC<AppProps> = ({
 }) => {
   return (
-
     <NextUIProvider>
       <ScoreBanner/>
     </NextUIProvider>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
