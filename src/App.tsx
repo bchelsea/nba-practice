1eeeef7e50d36bee42e9e3698c4631f6fab6b21c
Chelsea Barocio <cbarocio@C02C90M5MD6V.local>
2023-02-11 22:38:25 -0800

    Figuring out Next UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from '@nextui-org/react';
+import { ScoreBanner } from './components/ScoreBanner';
 
-function App() {
+interface AppProps {
+  [x: string]: string;
+}
+export const App: React.FC<AppProps> = ({
+}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <NextUIProvider>
+      <ScoreBanner/>
+    </NextUIProvider>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
